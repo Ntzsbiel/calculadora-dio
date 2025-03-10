baf9e39232d7eec39ad45feedeb1e10696239f4e
Seu Ntzsbiel <gabrielsilva.ilha186@gmail.com>
2025-03-09 22:02:19 -0300

    Style: Estilização dos inputs

---
+++ src/Components/Input/index.js
@@ -0,0 +1,11 @@
+import { InputContainer } from './styles';
+
+const MyInput= () => {
+  return (
+    <InputContainer>
+      <input/>
+    </InputContainer>
+  )
+}
+
+export default MyInput;
---
+++ src/Components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAfA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    
+    input{
+        width:100%;
+        height: 75px;
+        background-color: #AAAAFA;
+        border: 0;
+        padding: 0 10px;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFF;
+    }
+`
